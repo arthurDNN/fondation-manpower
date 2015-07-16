eeb992b780a7385c1a607568b511d487372616af
arthurDNN <adenonain@simplon.co>
2015-07-16 15:17:36 +0200

    test

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'manpower');
+define('DB_NAME', 'wp');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
